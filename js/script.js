ec7fa4f88c6d499036382f20186973c9484524c0
jhock9 <jon.hocker@gmail.com>
2021-07-18 15:39:05 -0500

    debug

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
     magic8Ball.listOfAnswers = ["It is Certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy, try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
     magic8Ball.askQuestion = function(question) { // define the method
       $("#8ball").attr("src", "img/magic8ball-back.png");
-      $("#answer").fadeIn(4000);
+      $("#answer").fadeIn(3000);
       var randomNumber = Math.random(); // create a random number
       var randomNumberArray = randomNumber * this.listOfAnswers.length; // make the random number between 0 and the number of items in your array
       var randomIndex = Math.floor(randomNumberArray); // round down the number to be a whole number
